091b9d87d2e65a0a85c2401a64827e50046a2635
Anusorn Mennakred <anusornleo@git.com>
2019-02-17 02:03:49 +0700

    add main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'fucku',
+      title: 'Assignment01',
       theme: ThemeData(
         brightness: Brightness.light,
         primaryColor: Colors.blue,
